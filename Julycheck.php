c5a00415e7e2fa9d56c56403a2aa4949786f42e6
Meijstro <35121706+Meijstro@users.noreply.github.com>
2018-01-31 12:39:58 +0100

    Add files via upload

---
+++ Julycheck.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+<title></title>
+<link rel="stylesheet" href=>
+<script type="text/javascript" src=""></script>
+</head>
+<body>
+
+
+<?php
+
+$m=date("m");
+
+if ($m==07) {
+  echo "It's July, so it can get really hot.";
+}
+    else {
+      echo "It’s not July, so at least we’re not in the peak of the heat.";
+    }
+ ?>
+
+</body>
+</html>
